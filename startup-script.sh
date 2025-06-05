a0a70879b74568b88402ee33491c9f5a7091fdbf
DuaneAbrames <106115024+DuaneAbrames@users.noreply.github.com>
2025-06-05 15:48:15 -0400

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -20,37 +20,6 @@ if [ ! -f /config/rsnapshot.conf ]; then
     cp /etc/rsnapshot.conf /config/rsnapshot.conf
 fi
 
-log "Replacing /etc/rsnapshot.conf with symlink to /config..."
-rm -f /etc/rsnapshot.conf
-ln -s /config/rsnapshot.conf /etc/rsnapshot.conf
-
-# 3. Create cron.daily and cron.weekly under /config
-log "Ensuring /config/cron.daily and /config/cron.weekly exist..."
-mkdir -p /config/cron.daily
-mkdir -p /config/cron.weekly
-
-# 4. Symlink cron.daily rsnapshot script
-if [ ! -f /config/cron.daily/rsnapshot ]; then
-    log "/config/cron.daily/rsnapshot is missing, touching it."
-    touch /config/cron.daily/rsnapshot
-fi
-log "Linking daily rsnapshot cron job to /config..."
-ln -sf /config/cron.daily/rsnapshot /etc/cron.daily/rsnapshot
-chmod 700 /config/cron.daily/rsnapshot
-
-# 5. Symlink weekly rsnapshot cron script
-if [ ! -f /config/cron.weekly/rsnapshot ]; then
-    log "/config/cron.weekly/rsnapshot is missing, touching it."
-    touch /config/cron.weekly/rsnapshot
-fi
-log "Linking weekly rsnapshot cron job to /config..."
-ln -sf /config/cron.weekly/rsnapshot /etc/cron.weekly/rsnapshot
-chmod 700 /config/cron.weekly/rsnapshot
-
-# 6. Update the cron job times based on the environment variables.  See comments in script.
-log "Running updatr-cron script"
-/bin/bash /root/update-cron.sh 
-
 # adding hosts file entries for our remote endpoint, if set.
 if [[ -v REMOTE_NAME && -v REMOTE_IP ]]; then
     log "found /config/hosts. Adding contents to /etc/hosts"
