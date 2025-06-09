199ee593f1b60b36d84fcee9cfe40b7ccd14f677
DuaneAbrames <106115024+DuaneAbrames@users.noreply.github.com>
2025-06-09 12:20:03 -0400

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -9,20 +9,24 @@ log() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $*"
 }
 
-
 # 1. Copy /root/startup-config to /config without overwriting
 log "Copying /root/startup-config to /config (without overwriting)..."
 cp -a --update=none /root/startup-config/* /config/
 
 # 2. Handle rsnapshot.conf
 if [ ! -f /config/rsnapshot.conf ]; then
-    log "Copying default rsnapshot.conf to /config..."
-    cp /etc/rsnapshot.conf /config/rsnapshot.conf
+    log "Copying eample rsnapshot.conf to /config..."
+    mv /config/rsnapshot.conf.example /config/rsnapshot.conf
+fi
+if [ -f /etc/rsnapshot.conf ]; then
+    log "Removing rsnapshot.conf from /etc and creating symlink to /config"
+    rm -f /etc/rsnapshot.conf
+    ln -s /config/rsnapshot.conf /etc/rsnapshot.conf
 fi
 
 # adding hosts file entries for our remote endpoint, if set.
 if [[ -v REMOTE_NAME && -v REMOTE_IP ]]; then
-    log "found /config/hosts. Adding contents to /etc/hosts"
+    log "Adding ${REMOTE_NAME} = ${REMOTE_IP} to /etc/hosts"
     echo "${REMOTE_IP}  ${REMOTE_NAME}" >>/etc/hosts
 fi
 
